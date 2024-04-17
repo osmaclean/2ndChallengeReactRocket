122a74582e1bdc7e6a99f8d1a789c09edbfe2fc9
Maclean <contatolucasmaclean@gmail.com>
2024-04-17 20:04:44 -0300

    feat: Removing unusable files, configuring page routing and styling structure

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,22 @@
+export const defaultTheme = {
+  white: '#FFF',
+
+  'gray-100': '#fafafa',
+  'gray-300': '#f3f2f2',
+  'gray-400': '#ededed',
+  'gray-500': '#e6e5e5',
+
+  'brown-100': '#d7d5d5',
+  'brown-200': '#8d8686',
+  'brown-300': '#574f4d',
+  'brown-400': '#403937',
+  'brown-500': '#272221',
+
+  'yellow-300': '#f1e9c9',
+  'yellow-400': '#dbac2c',
+  'yellow-500': '#c47f17',
+
+  'purple-300': '#ebe5f9',
+  'purple-400': '#8047f8',
+  'purple-500': '#4b2995',
+}
